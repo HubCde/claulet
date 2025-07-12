f951abfd6f89099496f971f475668143d13d6de0
Chuy <jescad27@gmail.com>
2025-07-11 22:48:56 -0600

    feat(admin): Cargar eventos en dashboard como componente (gestionEventos)

--- controllers/dashboard.controller.js
+++ controllers/dashboard.controller.js
@@ -1,4 +1,10 @@
-export const mostrarDashboardAdmin = (req, res) => res.render("dashboard_admin.ejs");
+import { cargarEventos } from "../models/eventos.models.js";
+
+export const mostrarDashboardAdmin = (req, res) => {
+    const eventos = cargarEventos();
+    res.render("dashboard_admin.ejs", {eventos})
+};
 export const mostrarDashboardHost = (req, res) => res.render('dashboard_host.ejs');
 export const mostrarDashboardOrganizador = (req, res) => res.render('dashboard_organizador.ejs');
 export const mostrarScanner = (req, res) => res.render('scanner.ejs');
+
